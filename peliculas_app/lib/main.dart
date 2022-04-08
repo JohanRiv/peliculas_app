072e477d3bdc3ff86b7bf764754c7deb368f910f
JohanRiv <johancamiloandres@hotmail.com>
2022-04-07 22:03:11 -0500

    Initial app settings

---
+++ peliculas_app/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'package:peliculas_app/screens/screens.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'YouMo',
+      initialRoute: 'home',
+      routes: {
+        'home': ((context) => HomeScreen()),
+        'details': ((context) => DetailsScreen()),
+      },
+    );
+  }
+}
